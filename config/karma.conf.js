597ea4f447c4827f2b66e586f2c590bc32e5e0aa
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-03-18 22:02:22 -0500

    test(ci): cross-browser testing w/ SauceLabs

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,5 +1,37 @@
 var path = require('path'),
-    webpackConfig = require('./webpack.config')
+    webpackConfig = require('./webpack.config'),
+    customLaunchers = {
+        sl_chrome: {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            platform: 'Windows 7',
+            version: 'latest'
+        },
+        sl_ios_safari: {
+            base: 'SauceLabs',
+            browserName: 'iphone',
+            platform: 'OS X 10.11',
+            version: '9.0'
+        },
+        sl_ios_safari: {
+            base: 'SauceLabs',
+            browserName: 'Safari',
+            platform: 'OS X 10.11',
+            version: '9.0'
+        },
+        sl_ie_11: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 8.1',
+            version: '11'
+        },
+        sl_edge: {
+            base: 'SauceLabs',
+            browserName: 'microsoftedge',
+            platform: 'Windows 10',
+            version: '13.10586'
+        }
+    }
 
 webpackConfig.module.loaders[0].query.plugins = ['rewire'];
 
@@ -24,7 +56,12 @@ module.exports = function (config) {
             preprocessors[path.resolve('test/tests.bundle.js')] = 'webpack'
             return preprocessors
         }()),
-        reporters: ['spec'],
+        sauceLabs: {
+            testName: 'Modern Uploader tests'
+        },
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
+        reporters: ['spec', 'saucelabs'],
         singleRun: true,
         webpack: webpackConfig,
         webpackMiddleware: {

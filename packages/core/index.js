03efc640af066f6c48ce036e92c4af48564e1f9e
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-03-29 10:22:36 -0500

    refactor(core): naming adjustment

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,6 +1,6 @@
 import Event from './src/event'
-import ModernUploaderCore from './src/core'
+import Core from './src/core'
 import Plugin from './src/plugin'
 
 export {Plugin, Event}
-export default ModernUploaderCore
+export default Core

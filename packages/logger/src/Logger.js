a7f8a1411377cee02e9825e1f7e5e599d3baf6fd
Ray Nicholus <github.com@garstasio.com>
2016-05-05 22:17:30 -0500

    feat(logger): constructor options

--- packages/logger/src/Logger.js
+++ packages/logger/src/Logger.js
@@ -7,6 +7,8 @@
  */
 import Plugin from '@modern-uploader/core/lib/plugin'
 
+const options = new WeakMap()
+
 /**
  * Main class for the Modern Uploader logger plug-in.
  *
@@ -14,7 +16,34 @@ import Plugin from '@modern-uploader/core/lib/plugin'
  * @since 0.0.0
  */
 class Logger extends Plugin {
-    
+    /**
+     * Create a new instance of the Logger plug-in. The plug-in can be configured
+     * by passing various options at this point.
+     *
+     * @param {Object} options - Various options used to configure the plug-in.
+     * @param {boolean} [options.logAllEvents=false] - Log all events that progress through the system (even cancelled ones).
+     * @param {string} [options.minimumLevel='info'] - Only log events with this severity level or greater.
+     * @param {boolean} [options.timestamp=true] - Include the current time when logging the event.
+     * @since 0.0.0
+     * @example
+     * import Core from '@modern-uploader/core'
+     * import Logger from '@modern-uploader/logger'
+     * // insert Logger plug-in into system with all default options
+     * const modernUploader = new Core([
+     *    new Logger()
+     * ])
+     *
+     * import Core from '@modern-uploader/core'
+     * import Logger from '@modern-uploader/logger'
+     * // insert Logger plug-in into system - ensure it also logs all events
+     * const modernUploader = new Core([
+     *    new Logger({logAllEvents: true})
+     * ])
+     */
+    constructor({logAllEvents = false, minimumLevel = 'info', timestamp = true} = {}) {
+        super()
+        options.set(this, {logAllEvents, minimumLevel, timestamp})
+    }
 }
 
 export default Logger
